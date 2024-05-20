a2184377921b5201a22c2cac10f33212791cfc78
delegatkyan <delegatkyan@gmail.com>
2024-05-20 10:51:25 +0000

    Testing

--- src/main/kotlin/best/spaghetcodes/duckdueller/core/Config.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/core/Config.kt
@@ -2,7 +2,6 @@ package best.spaghetcodes.duckdueller.core
 
 import best.spaghetcodes.duckdueller.DuckDueller
 import best.spaghetcodes.duckdueller.bot.bots.*
-import best.spaghetcodes.duckdueller.utils.ChatUtils
 import gg.essential.vigilance.Vigilant
 import gg.essential.vigilance.data.Property
 import gg.essential.vigilance.data.PropertyType
@@ -18,16 +17,16 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
     @Property(
         type = PropertyType.SELECTOR,
         name = "Current Bot",
-        description = "The bot you want to use",
+        description = "Select the bot you want to use",
         category = "General",
-        options = ["Sumo", "Boxing", "Classic", "OP", "Combo"]
+        options = ["Boxing", "OP", "SUMO", "CLASSIC",]
     )
     val currentBot = 0
 
     @Property(
         type = PropertyType.TEXT,
         name = "API Key",
-        description = "This account's API key, can also be set using \"/api new\".",
+        description = "Get your API Key <a href="https://developer.hypixel.net/">Here</a>".",
         placeholder = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
         category = "General",
     )
@@ -42,10 +41,10 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
     val lobbyMovement = true
 
     @Property(
-    type = PropertyType.SWITCH,
-    name = "Disable Chat Messages",
-    description = "When this is enabled, the bot will not send any chat messages.",
-    category = "General",
+        type = PropertyType.SWITCH,
+        name = "Disable Chat Messages",
+        description = "When this is enabled, the bot will not send any chat messages.",
+        category = "General",
     )
     val disableChatMessages = false
 
@@ -55,13 +54,12 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         description = "After X games the bot will underperform and throw the game. 0 = disabled.",
         category = "General",
         min = 0,
-        max = 1000,
-        increment = 10
+        max = 1000
     )
     val throwAfterGames = 0
 
     @Property(
-        type = PropertyType.SLIDER,
+        type = PropertyType.NUMBER,
         name = "Disconnect After X Games",
         description = "After X games the bot will toggle off and disconnect. 0 = disabled.",
         category = "General",
@@ -76,8 +74,7 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         description = "After X minutes the bot will toggle off and disconnect. 0 = disabled",
         category = "General",
         min = 0,
-        max = 500,
-        increment = 30
+        max = 500
     )
     val disconnectAfterMinutes = 0
 
@@ -90,9 +87,8 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         name = "Min CPS",
         description = "The minimum CPS that the bot will be clicking at.",
         category = "Combat",
-        min = 6,
-        max = 15,
-        increment = 1
+        min = 0,
+        max = 20
     )
     val minCPS = 10
 
@@ -101,9 +97,8 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         name = "Max CPS",
         description = "The maximum CPS that the bot will be clicking at.",
         category = "Combat",
-        min = 9,
-        max = 18,
-        increment = 1
+        min = 0,
+        max = 28
     )
     val maxCPS = 14
 
@@ -112,9 +107,8 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         name = "Horizontal Look Speed",
         description = "How fast the bot can look left/right (lower number = less snappy, slightly less accurate when teleporting)",
         category = "Combat",
-        min = 5,
-        max = 15,
-        increment = 1
+        min = 3,
+        max = 20
     )
     val lookSpeedHorizontal = 10
 
@@ -124,18 +118,17 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         description = "How fast the bot can look up/down (lower number = less snappy, slightly less accurate when teleporting)",
         category = "Combat",
         min = 1,
-        max = 15,
-        increment = 1
+        max = 15
     )
     val lookSpeedVertical = 5
 
     @Property(
-        type = PropertyType.DECIMAL_SLIDER,
+        type = PropertyType.DECIMAL,
         name = "Look Randomization",
         description = "How much randomization should happen when looking (higher number = more jittery aim)",
         category = "Combat",
-        minF = 0f,
-        maxF = 2f,
+        min = 0f,
+        max = 2f
     )
     val lookRand = 0.3f
 
@@ -145,8 +138,7 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         description = "How close the opponent has to be before the bot starts tracking them",
         category = "Combat",
         min = 120,
-        max = 180,
-        increment = 5
+        max = 180
     )
     val maxDistanceLook = 150
 
@@ -156,8 +148,7 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         description = "How close the opponent has to be before the bot starts attacking them",
         category = "Combat",
         min = 5,
-        max = 15,
-        increment = 1
+        max = 15
     )
     val maxDistanceAttack = 5
 
@@ -187,8 +178,7 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         description = "How long to wait after the game before sending the message",
         category = "AutoGG",
         min = 50,
-        max = 1000,
-        increment = 50
+        max = 1000
     )
     val ggDelay = 100
 
@@ -214,8 +204,7 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         description = "How long to wait before sending the start message",
         category = "AutoGG",
         min = 50,
-        max = 1000,
-        increment = 50
+        max = 1000
     )
     val startMessageDelay = 100
 
@@ -229,8 +218,7 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         description = "How long to wait after a game before re-queueing",
         category = "Auto Requeue",
         min = 500,
-        max = 5000,
-        increment = 50
+        max = 5000
     )
     val autoRqDelay = 2500
 
@@ -240,8 +228,7 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         description = "How long to wait before re-queueing if no game starts",
         category = "Auto Requeue",
         min = 15,
-        max = 60,
-        increment = 5
+        max = 60
     )
     val rqNoGame = 30
 
@@ -274,7 +261,7 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
     val enableDodging = true
 
     @Property(
-        type = PropertyType.SLIDER,
+        type = PropertyType.NUMBER,
         name = "Dodge Wins",
         description = "How many wins a player can have before being dodged",
         category = "Queue Dodging",
@@ -289,18 +276,17 @@ class Config : Vigilant(File(DuckDueller.configLocation), sortingBehavior = Conf
         description = "How large a player's winstreak can be before being dodged",
         category = "Queue Dodging",
         min = 10,
-        max = 100,
-        increment = 5
+        max = 100
     )
     val dodgeWS = 15
 
     @Property(
-        type = PropertyType.DECIMAL_SLIDER,
+        type = PropertyType.DECIMAL,
         name = "Dodge W/L",
         description = "How large a player's w/l ratio can be before being dodged",
         category = "Queue Dodging",
-        minF = 2f,
-        maxF = 15f,
+        min = 2f,
+        max = 15f
     )
     val dodgeWLR = 3.0f
 
